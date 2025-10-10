842eecb7045b8f42f50fe785ccbaf9b3eb38f839
robert9r3 <rvs128652@gmail.com>
2025-10-10 07:48:29 -0300

    onkey

---
+++ main.js
@@ -0,0 +1,30 @@
+function tocaSomPom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play;
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+}
+
+const listadeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listadeTeclas.length; contador++){
+    const tecla = listadeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function (){
+        tocaSomPom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+}
+
+listadeTeclas[0].onclick = tocaSomPom;
